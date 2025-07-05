18b4419a5579887bf01e22143b517549ff875c92
Anjali <sj5065070@gmail.com>
2025-07-05 23:54:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './comp/Todo'
-
+import   { Toaster } from 'react-hot-toast';
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
     <Todo/>
+   <Toaster position="top-center" reverseOrder={false} />
     </>
   )
 }
