62a2bf20ce8140a09bd0a0b324c633745f024c80
Anjali <sj5065070@gmail.com>
2025-07-05 20:17:34 +0530

    Update Todo.jsx

--- src/comp/Todo.jsx
+++ src/comp/Todo.jsx
@@ -7,11 +7,11 @@ function Todo() {
   
 
 let todotask=[
-  {classA:'bca',compelete:true},
-  {classA:'mca',compelete:true},
+  {classA:'bca',compelete:false},
+  {classA:'mca',compelete:false},
 ];
 
-let [task,settask]=useState("");
+let [task,settask]=useState(" ");
 
 let [todoall,settodo]=useState(todotask)
 
@@ -20,25 +20,37 @@ let [todoall,settodo]=useState(todotask)
 
 function handlesubmit(e){
   e.preventDefault();
-  console.log("task:",task)
+ // console.log("task:",task)
 
- settodo([...todoall,{classA:task,compelete:false}])
+ settodo([...todoall,{classA:task,compelete:true}])
+ settask(" ")
 }
 
 function handlecheck(id){
   let copyCheck=[...todoall]
-  copyCheck[id].compelete=!copyCheck[id].compelete;
-  settodo[copyCheck]
-  console.log(id)
+  copyCheck[id].compelete = !copyCheck[id].compelete;
+  settodo(copyCheck)
 }
+
+function handledelete(id){
+//  console.log(id)
+let copydeletecheck=[...todoall]
+let filtervalue = copydeletecheck.filter((value,index)=>{
+  return index!==id;
+    
+  })
+  //console.log(filtervalue)
+  settodo(filtervalue)
+}
+
 
   return (
     <>
     <div className="conatiner">
       <div className="row">
         <div className="col-md-12">
-    <div class="border border-danger-subtle px-5 py-2 rounded shadow p-3 mb-5 bg-white rounded ">
-    <h2 class="text-center">To-Do App🚀</h2>
+    <div className="border border-danger-subtle px-5 py-3 rounded shadow p-3 mb-5 bg-white rounded ">
+    <h2 className="text-center">To-Do App🚀</h2>
     <form action="" onSubmit={handlesubmit}>
       <div>
         <input type="text" placeholder='add task here...'
@@ -47,24 +59,36 @@ function handlecheck(id){
         />
        <button>Add Task</button>
       </div>
-    </form>
+  
     
-    {
+    { todoall.length===0? alert("no tast added😐"):
       todoall.map((item,index)=>(
-       <ul key={index}>
-          <input type="checkbox" name="" id="checkbox" 
-          checked={item.compelete}
-          onClick={()=>(
-            handlecheck(index)
-      )}
-          /><span>{item.classA}</span>
+       <ul key={index} className="justify-content-between px-3 py-1">
+          <div>
+            <input type="checkbox" id="checkbox" 
+             checked={item.compelete}
+             onClick={()=>{
+             handlecheck(index)
+          }}
+          /><span 
+           style={{
+                    textDecoration: item.compelete ? "line-through" : "none",}}
+                    >{item.classA}</span>
+          </div>
+          <div><i className="bi bi-trash3" 
+           onClick={()=>{
+            handledelete(index)
+          }}
+           ></i></div>
        </ul>
       ))
     }
+    </form>
    </div>
    </div>
    </div>
    </div>
+    
     </>
   )
 }
